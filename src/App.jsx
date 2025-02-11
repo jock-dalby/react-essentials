95953d706a39a5c6864875084ad6805fdb4bec4c
Jock Dalby <jockdalby86@gmail.com>
2025-02-12 09:29:40 +1000

    create TabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {CORE_CONCEPTS} from './data'
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -13,7 +14,12 @@ function App() {
             {CORE_CONCEPTS.map(props => (<CoreConcept {...props}/>))}
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
