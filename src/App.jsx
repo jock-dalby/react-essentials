028d458ff5e14dec0b296624a0630a06064f1b76
Jock Dalby <jockdalby86@gmail.com>
2025-02-12 10:39:00 +1000

    show selected topic details in tab-content section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 
-import {CORE_CONCEPTS} from './data'
+import {CORE_CONCEPTS, EXAMPLES} from './data'
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
 
 function App() {
   
-  const [tabContent, setTabContent] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   const handleClick = (tabLabel) => {
-    setTabContent(tabLabel);
+    setSelectedTopic(tabLabel.toLowerCase());
   }
 
   return (
@@ -20,15 +20,21 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map(props => (<CoreConcept {...props}/>))}
+            {CORE_CONCEPTS.map(props => (<CoreConcept key={props.title} {...props}/>))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            {['Components', 'JSX', 'Props', 'State'].map(label => <TabButton onClick={() => handleClick(label)}>{label}</TabButton>)}
+            {Object.values(EXAMPLES).map(({title}) => <TabButton key={title} onClick={() => handleClick(title)}>{title}</TabButton>)}
           </menu>
-          {tabContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
