9ded574e3aa722a3c6917be357e5ea997788ee78
Jock Dalby <jockdalby86@gmail.com>
2025-02-13 10:17:20 +1000

    create and use Tabs component

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Section from "./Section";
 import TabButton from "./TabButton";
+import Tabs from "./Tabs";
 import { EXAMPLES } from "../data";
 
 const Examples = () => {
@@ -9,19 +10,20 @@ const Examples = () => {
     const handleClick = (tabLabel) => {
         setSelectedTopic(tabLabel);
     }
+
+    const buttons = Object.keys(EXAMPLES).map((title) => <TabButton key={title} isActive={selectedTopic === title} onClick={() => handleClick(title)}>{EXAMPLES[title].title}</TabButton>);
         
     return (
         <Section id="examples" title="Examples">
-          <menu>
-            {Object.keys(EXAMPLES).map((title) => <TabButton key={title} isActive={selectedTopic === title} onClick={() => handleClick(title)}>{EXAMPLES[title].title}</TabButton>)}
-          </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          <Tabs buttons={buttons}>
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+          </Tabs>
         </Section>
     )   
 }
